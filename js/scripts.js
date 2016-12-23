ef0f544172b515865f490ce7f68bde2a4b0a490b
Drew Parker <drew.i.parker@gmail.com>
2016-12-23 15:56:22 -0500

    Merged from branch

--- js/scripts.js
+++ js/scripts.js
@@ -2,66 +2,63 @@ $(function() {
     var apiBaseUrl = 'https://api.themoviedb.org/3/';
     var encoding = '&language=en-US&page=1&include_adult=false';
     var apiImageUrl = 'https://image.tmdb.org/t/p/';
-    var apiSearch = `${apiBaseUrl}search/movie?api_key=${movieKey}&${encoding}&query=`;
+
 
     const nowPlayingUrl = `${apiBaseUrl}movie/now_playing?api_key=${movieKey}&${encoding}`;
     const upComingUrl = `${apiBaseUrl}movie/upcoming?api_key=${movieKey}&${encoding}`;
 
+    // This function calls first so we have movies displayed before the user
+    // has searched.
+    generateCards(upComingUrl);
 
-    $.getJSON(nowPlayingUrl, function(nowPlayingData) {
+    // The main query function: makes an API call to tmdb, gets movies, and
+    // creates html of cards featuring posters and a view trailer button.
+    function generateCards(queryUrl) {
 
-        var movies = nowPlayingData.results;
-        var nowPlayingHTML = '';
+        $.getJSON(queryUrl, function(movieData) {
+            var movies = movieData.results;
+            var movieCardHtml = '';
 
-        for (let movie of movies) {
-            var id = movie.id;
-            var youTubeUrl = 'https://youtu.be/';
-            var trailerUrl = `${apiBaseUrl}movie/${id}/videos?api_key=${movieKey}&language=en-US`;
+            for (let movie of movies) {
+                let id = movie.id;
+                var poster = apiImageUrl + 'w300' + movie.poster_path;
+                // Construct an API url from each movie ID to get more information
+                // var movieUrl = `${apiBaseUrl}movie/${id}?api_key=${movieKey}&${encoding}&append_to_response=credits,release_dates`;
+                // $.getJSON()
+                movieCardHtml += `<div class="col-sm-3 movie-card" id="${id}">`;
+                    movieCardHtml += `<img src="${poster}">`;
+                    movieCardHtml += `<div class="trailer-btn-wrapper">`
+                        movieCardHtml += `<button class="btn btn-primary trailer-btn" id="${id}" data-toggle="modal" data-target=".trailer-modal">View trailer</button>`;
+                    movieCardHtml += '</div>';
+                movieCardHtml += '</div>';
+            }
+            $('#movie-grid').html(movieCardHtml);
 
-            $.getJSON(trailerUrl, function(trailerData) {
-                var poster = apiImageUrl + 'w600' + movie.poster_path;
-                var trailer = youTubeUrl + trailerData.results[0].key;
-
-                // Build the html with clickable image posters that direct to youtube trailer
-                nowPlayingHTML += `<div class="col-sm-3 movie-card">`;
-                    nowPlayingHTML += `<a href="${trailer}"><img src="${poster}"></a>`;
-                    nowPlayingHTML += `<div class="trailer-btn-wrapper">`
-                        nowPlayingHTML += `<button class="btn btn-primary trailer-btn" id="${id}">View trailer</button>`;
-                    nowPlayingHTML += '</div>';
-                nowPlayingHTML += '</div>';
-
-                $('#movie-grid').html(nowPlayingHTML);
-
-                $('.trailer-btn').click(function() {
-                    console.log(this);
+            $('.trailer-btn').click(function() {
+                modalHTML = '';
+                let id = $(this).attr('id');
+                var trailerUrl = `${apiBaseUrl}movie/${id}/videos?api_key=${movieKey}&language=en-US`;
+                $.getJSON(trailerUrl, function(trailerData) {
+                    var youTubeUrl = trailerData.results[0].key;
+                    var trailer = `https://www.youtube.com/embed/${youTubeUrl}?autoplay=1`;
+                    var iFrameTrailer = `<iframe width="600" height="355" src="${trailer}" frameborder="0"></iframe>`;
+                    $('.modal-content').html(iFrameTrailer);
                 });
-
             });
-        }
+        });
+    }
+
+    // The main search controller.
+    $('.movie-search').submit(function() {
+        event.preventDefault();
+
+        var searchTerm = $('#movie-input').val();
+        var searchQueryUrl = `${apiBaseUrl}search/movie?api_key=${movieKey}&${encoding}&query=${searchTerm}`;
+        console.log(searchTerm);
+        console.log(searchQueryUrl);
+        generateCards(searchQueryUrl);
     });
 
 
-
-    // var trailer = youTubeUrl + videoData.results[0].key;
-    // nowPlayingHTML += `<div class="col-sm-3" id="${id}">`;
-    //     nowPlayingHTML += `<a href="${trailer}"><img src="${poster}"></a>`;
-    // nowPlayingHTML += '</div>';
-
-    // var trailerPromise = $.getJSON(trailerUrl);
-    // var trailerLink = trailerPromise.done(function(videoData) {
-    //     var trailer = youTubeUrl + videoData.results[0].key;
-    // });
-    //
-    // var trailerUrl = `${apiBaseUrl}movie/${id}/videos?api_key=${movieKey}&language=en-US`;
-
-    // $('.movie-form').submit(function() {
-    //     event.preventDefault();
-    //     var movieSearched = $('.movie-input').val();
-    //     console.log(movieSearched);
-    //     var movieQuery = apiSearch + movieSearched;
-    //     console.log(movieQuery);
-    //     $.getJSON(movieQuery, function(movieData) {
-    //         console.log(movieData);
-    //     });
-    // });
+// Closes the doc ready function
 });
